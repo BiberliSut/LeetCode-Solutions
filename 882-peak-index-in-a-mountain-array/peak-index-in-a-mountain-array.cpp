da977e29774dbec84b24c8788cada910f1e9acdc
Erdem Erçetin <102034441+BiberliSut@users.noreply.github.com>
2023-07-25 22:03:25 +0300

    Time: 132 ms (35.53%) | Memory: 59.6 MB (88.25%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n= arr.size();
+        for(int i=0; i<n-1; i++){
+            if(arr[i]>arr[i+1])
+                return i;
+        }
+        return -1;
+        //ingilizce isimleri istiyor, diziler falan yine ayni
+    }
+};
